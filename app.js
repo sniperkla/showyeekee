c13bcdc2a93349f99354516fdf66ebc0660c3456
Your Name <you@example.com>
2023-04-14 16:01:17 +0700

    sss

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const Schema = require("./schema");
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.static("public"));
+
+mongoose
+  .connect("mongodb://147.50.227.164:27017/data", { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Could not connect to MongoDB", err));
+
+app.get("/yeekee", async (req, res) => {
+  Schema.find()
+    .then((result) => {
+      res.status(200).send(result);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+});
+app.listen(3002, () => {
+  console.log("Server listening on port 3002");
+});
